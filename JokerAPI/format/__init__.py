b092577722681d5dd460467543f37701077e598e
vz <71209712+0x96e63@users.noreply.github.com>
2023-11-02 15:48:08 +0000

    [`0.039`] > Release, re factor.

--- JokerAPI/format/__init__.py
+++ JokerAPI/format/__init__.py
@@ -4,3 +4,18 @@ def __dial_format__(apiKey, baseUrl: str, to: str, from_: str, callbackUrl: str)
 
 def __hangup_format__(apiKey, baseUrl: str, sid: str) -> str:
     return f"{baseUrl}?apikey={apiKey}&callsid={sid}"
+
+def __play_audio_format__(apiKey, baseUrl: str, sid: str, audioUrl: str) -> str:
+    return f"{baseUrl}?apikey{apiKey}&callsid={sid}&audiourl={audioUrl}"
+
+def __play_text_format__(apiKey, baseUrl: str, sid: str, text: str, voice: str) -> str:
+    return f"{baseUrl}?apikey={apiKey}&callsid={sid}&text={text}&voice={voice}"
+
+def __play_audio_dtmf__(apiKey, baseUrl: str, sid: str, audioUrl: str, maxDigits: int) -> str:
+    return f"{baseUrl}?apikey={apiKey}&callsid={sid}&audiourl={audioUrl}&maxdigits={maxDigits}"
+
+def __play_text_dtmf__(apiKey, baseUrl: str, sid: str, text: str, voice: str, maxDigits: int) -> str:
+    return f"{baseUrl}?apikey={apiKey}&callsid={sid}&text={text}&voice={voice}&maxdigits={maxDigits}"
+
+def __send_dtmf_format__(apiKey, baseUrl: str, sid: str, input_) -> str:
+    return f"{baseUrl}?apikey={apiKey}&callsid={sid}&input={input_}"
